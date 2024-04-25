b8f2925ea8e14b51bc1d38240828828cb2b699f1
Aleksei Chashchegorov <chashchegorov@gmail.com>
2024-04-25 19:37:26 +0200

    deriving initial sliding window

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -1,6 +1,16 @@
 #include "Solution.h"
 
-std::vector<double> Solution::medianSlidingWindow(const std::vector<int>& /*nums*/, int /*k*/) {
-	// TODO: implementation
+
+#include <iostream>
+
+std::multiset<int> Solution::initialSlidingWindow(const std::vector<int>& nums, int k) {
+	return std::multiset<int>(nums.begin(), std::next(nums.begin(), k));
+}
+
+std::vector<double> Solution::medianSlidingWindow(const std::vector<int>& nums, int k) {
+	// TODO: completed implementation
+	for (const auto& m : initialSlidingWindow(nums, k)) {
+		std::cout << m << " ";
+	} std::cout << std::endl;
 	return {};
 }
--- src/Solution.h
+++ src/Solution.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <vector>
+#include <set>
 
 class Solution {
+private:
+    static std::multiset<int> initialSlidingWindow(const std::vector<int>& nums, int k);
 public:
     std::vector<double> medianSlidingWindow(const std::vector<int>& nums, int k);
 };
