b239b68338696a6931a4e45bbec436d961d17a75
Aleksei Chashchegorov <chashchegorov@gmail.com>
2024-04-25 22:26:05 +0200

    sliding window moving with text representations

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -12,13 +12,68 @@ SlidingWindow Solution::initialSlidingWindow(const Nums& nums, int k) {
 	return std::make_tuple(std::move(window), std::move(back), 0);
 }
 
+SlidingWindow& Solution::move(SlidingWindow& window, const Nums& nums) {
+	auto& [front_part, back_part, start_index] = window;
+	
+	const auto window_size = front_part.size() + back_part.size();
+	const auto required_back_part_size = back_part.size();
+
+	
+	// Erasing
+	const auto value_to_remove = nums[start_index++];
+	if(const auto it_at_front = front_part.find(value_to_remove); it_at_front != front_part.end()) {
+		front_part.erase(it_at_front);
+	} else {
+		auto it_at_back = back_part.find(value_to_remove);
+		back_part.erase(it_at_back);
+	}
+
+	// Adding
+	const auto value_to_add = nums[start_index + window_size - 1];
+	if(!back_part.empty()) {
+		if (*back_part.begin() > value_to_add)
+			front_part.emplace(value_to_add);
+		else
+			back_part.emplace(value_to_add);
+	} else if (!front_part.empty()) {
+		if (*front_part.rbegin() <= value_to_add)
+			front_part.emplace(value_to_add);
+		else
+			back_part.emplace(value_to_add);
+
+	} else {
+		back_part.emplace(value_to_add);
+	}
+	
+	// Balanicing
+	while (back_part.size() != required_back_part_size) {
+		if (back_part.size() < required_back_part_size) {
+			back_part.emplace(*front_part.rbegin());
+			front_part.erase(std::prev(front_part.end())); // convert to forward iterator
+		} else {
+			front_part.emplace(*back_part.begin());
+			back_part.erase(back_part.begin());
+		}
+	}
+
+	
+	return window;
+}
+
+
 Medians Solution::medianSlidingWindow(const Nums& nums, int k) {
 	// TODO: completed implementation
-	auto [front, back, start_index] = initialSlidingWindow(nums, k);
+	auto window = initialSlidingWindow(nums, k);
 
-	for (const auto& m : front) { std::cout << m << " "; } std::cout << std::endl;
-	for (const auto& m : back) { std::cout << m << " "; } std::cout << std::endl;
-        std::cout << start_index << std::endl;
+	std::cout << "{" ; for (const auto& m : std::get<0>(window)) { std::cout << m << " "; } std::cout << "}";
+	std::cout << "{" ; for (const auto& m : std::get<1>(window)) { std::cout << m << " "; } std::cout << "}" << std::endl;
+        std::cout << std::get<2>(window) << std::endl;
+
+	for(auto steps_count = nums.size() - k; steps_count !=0; steps_count--) {
+		std::cout << "{" ; for (const auto& m : std::get<0>(move(window, nums))) { std::cout << m << " "; } std::cout << "}";
+		std::cout << "{" ; for (const auto& m : std::get<1>(window)) { std::cout << m << " "; } std::cout << "}" << std::endl;
+        	std::cout << std::get<2>(window) << std::endl;
+	}
 
 	return {};
 }
--- src/Solution.h
+++ src/Solution.h
@@ -5,6 +5,7 @@
 class Solution {
 private:
     static SlidingWindow initialSlidingWindow(const Nums& nums, int k);
+    static SlidingWindow& move(SlidingWindow& window, const Nums& nums);
 public:
     Medians medianSlidingWindow(const Nums& nums, int k);
 };
